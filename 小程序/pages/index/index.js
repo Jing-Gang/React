756938d361bedaaf66f979aa95050efc3f29d822
Shi JingGang <2966437988@qq.com>
2019-11-19 20:58:14 +0800

    小程序排版

---
+++ 小程序/pages/index/index.js
@@ -0,0 +1,46 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data:{
+    banners:[
+      '/assets/coffee.jpg',
+      '/assets/coffee.jpg',
+      '/assets/coffee.jpg',
+      '/assets/coffee.jpg',
+    ],
+    list:[
+      {
+        id: 1,
+        title: '大傻狗',
+        image: '/assets/coffee.jpg'
+      },
+      {
+        id: 1,
+        title: '圣诞奇妙味',
+        image: '/assets/coffee.jpg'
+      },
+      {
+        id: 1,
+        title: '圣诞奇妙味',
+        image: '/assets/coffee.jpg'
+      },
+      {
+        id: 1,
+        title: '圣诞奇妙味',
+        image: '/assets/coffee.jpg'
+      },
+      {
+        id: 2,
+        title: '圣诞奇妙味',
+        image: '/assets/coffee.jpg'
+      },
+      {
+        id: 6,
+        title: '圣诞奇妙味',
+        image: '/assets/coffee.jpg'
+      }
+    ]
+  }
+})
